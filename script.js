317a8c98ea80cd26f1f943601680fb9f2141e5fc
Darius Mahjoob <67082003+DMahjoob@users.noreply.github.com>
2025-06-25 13:19:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -110,11 +110,8 @@ document.addEventListener('DOMContentLoaded', function() {
       if (i < originalText.length) {
         heroTitle.textContent += originalText.charAt(i);
         i++;
-        setTimeout(typeWriter, 100);
       }
     }
-    
-    setTimeout(typeWriter, 500);
   }
 });
 
