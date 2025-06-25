90a14bd321bfb961482c03fa3de1d4afdf65f940
Darius Mahjoob <67082003+DMahjoob@users.noreply.github.com>
2025-06-25 13:27:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   }
-  setTimeout(typeWriter, 1000); // Loops the typewriter effect
+  setTimeout(typeWriter, 200); // Loops the typewriter effect
 });
 
 // Add active state styles
